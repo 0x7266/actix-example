79bf2f9aa1d3a5fd7c3d89b19dac7cb2123b5946
rafael <rrrr0ffff@gmail.com>
2024-05-13 23:50:54 -0300

    add db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use serde::Deserialize;
 use serde::Serialize;
+use std::collections::HashMap;
 use std::io::Result;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 use actix_web::{web, App, HttpServer, Responder};
+
+type UserDb = Arc<Mutex<HashMap<u32, User>>>;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
@@ -24,9 +29,14 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> Result<()> {
     println!("Starting server on port 3333");
-    HttpServer::new(|| App::new().service(index).service(greet))
-        .bind(("localhost", 3333))?
-        .workers(2)
-        .run()
-        .await
+
+    let user_db: UserDb = Arc::new(Mutex::new(HashMap::<u32, User>::new()));
+    HttpServer::new(move || {
+        let app_data = web::Data::new(user_db.clone());
+        App::new().app_data(app_data).service(index).service(greet)
+    })
+    .bind(("localhost", 3333))?
+    .workers(2)
+    .run()
+    .await
 }
